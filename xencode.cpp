a9709d067db739ca86897f9b914f88f868b9993f
凌潇 李 <571857324@qq.com>
2025-05-20 20:02:15 +0800

    完成了-XEncode编码封装 1、编码和AVFrame创建接口 2、解决丢帧-添加获取编码缓冲区接口

--- xencode.cpp
+++ xencode.cpp
@@ -1,13 +1,14 @@
 #include "xencode.h"
 #include <iostream>
 using namespace std;
-extern"C"
-{
+extern"C"{
 #include <libavcodec/avcodec.h>
 #include <libavutil/opt.h>
 }
 #pragma comment(lib,"avcodec.lib")
 #pragma comment(lib,"avutil.lib")
+
+//打印错误函数
 static void PrintErr(int err) 
 {
 	char buf[1024] = { 0 };
@@ -15,7 +16,7 @@ static void PrintErr(int err)
 	cerr << buf << endl;
 }
 
-AVCodecContext* xencode::Create(int codec_id)
+AVCodecContext* XEncode::Create(int codec_id)
 {
 	//1 找到编码器  AV_CODEC_ID_HEVC(H265)
 	auto codec = avcodec_find_encoder((AVCodecID)codec_id);
@@ -38,7 +39,104 @@ AVCodecContext* xencode::Create(int codec_id)
 	return ctx;
 }
 
-void xencode::set_c(AVCodecContext* ctx)
+bool XEncode::Open()
+{
+	unique_lock<mutex>lock(mux_);
+	if (!ctx_)return false;
+	///4 打开编码上下文
+	auto re = avcodec_open2(ctx_, NULL, NULL);
+	if (re != 0) {
+		PrintErr(re);
+		return false;
+	}
+	return true;
+}
+
+AVFrame* XEncode::CreateFrame()
+{
+	unique_lock<mutex>lock(mux_);
+	if (!ctx_)return nullptr;
+
+	auto frame = av_frame_alloc();
+	/// 创建好AVFrame空间 未压缩数据
+	frame->width = ctx_->width;
+	frame->height = ctx_->height;
+	frame->format = ctx_->pix_fmt;
+	auto re = av_frame_get_buffer(frame, 0);
+	if (re != 0)
+	{
+		av_frame_free(&frame);
+		PrintErr(re);
+		return nullptr;
+	}
+	return frame;
+}
+
+AVPacket* XEncode::Encode(const AVFrame* frame)
+{
+	unique_lock<mutex>lock(mux_);
+	if (!ctx_)return nullptr;
+
+	auto re = avcodec_send_frame(ctx_, frame);
+	if (re != 0)return nullptr;
+
+	AVPacket* pkt = av_packet_alloc();
+
+	//循环接收所有可用的包
+	//while (re >= 0) {
+	//	re = avcodec_receive_packet(enc_ctx, pkt);
+	//	if (re == AVERROR(EAGAIN) || re == AVERROR_EOF)
+	//		break;
+	//	// 写pkt到文件或发送
+	//	av_packet_unref(pkt);
+	//}
+
+	re = avcodec_receive_packet(ctx_, pkt);
+	if (re == 0) 
+	{
+		return pkt;
+	}
+	av_packet_free(&pkt);
+	if (re == AVERROR(EAGAIN) || re == AVERROR_EOF)
+	{
+		return nullptr;
+	}
+	if (re < 0)
+	{
+		PrintErr(re);
+	}
+
+	return nullptr;
+}
+
+vector<AVPacket*> XEncode::End()
+{
+	vector<AVPacket*> res;
+	unique_lock<mutex>lock(mux_);
+	if (!ctx_)return res;
+
+	// 发送NULL帧，通知编码器输出缓冲区中所有剩余的数据包
+	auto re = avcodec_send_frame(ctx_, NULL);
+	if (re != 0)return res;
+
+	// 循环获取所有缓冲区中的AVPacket
+	while (re >= 0)
+	{
+		AVPacket* pkt = av_packet_alloc();
+		re = avcodec_receive_packet(ctx_, pkt);
+		if (re != 0)
+		{
+			av_packet_free(&pkt);
+			break;
+		}
+		res.push_back(pkt);
+	}
+	return res;
+}
+
+
+
+void XEncode::set_c(AVCodecContext* ctx)
 {
 	unique_lock<mutex> lock(mux_);
 	if (ctx)
@@ -48,7 +146,7 @@ void xencode::set_c(AVCodecContext* ctx)
 	this->ctx_ = ctx;
 }
 
-bool xencode::SetOpt(const char* key, const char* val)
+bool XEncode::SetOpt(const char* key, const char* val)
 {
 	unique_lock<mutex> lock(mux_);
 	if (!ctx_)return false;
@@ -61,7 +159,7 @@ bool xencode::SetOpt(const char* key, const char* val)
 	return true;
 }
 
-bool xencode::SetOpt(const char* key, int val)
+bool XEncode::SetOpt(const char* key, int val)
 {
 	unique_lock<mutex> lock(mux_);
 	if (!ctx_)return false;
--- xencode.h
+++ xencode.h
@@ -1,15 +1,39 @@
 #pragma once
 #include <mutex>
+#include <vector>
 struct AVCodecContext;
-
-class xencode
+struct AVFrame;
+struct AVPacket;
+class XEncode
 {
+public:
+
    //////////////////////////////////////////
    /// 创建编码上下文
    /// 
    /// @para codec_id 编码器ID号，对应ffmpeg
    /// @return 编码上下文 ,失败返回nullptr
     static AVCodecContext* Create(int codec_id);
+
+
+    //////////////////////////////////////////
+    /// 打开编码器
+    bool Open();
+
+    ///////////////////////////////////////////////////////////////
+    //根据AVCodecContext 创建一个AVFrame，需要调用者释放av_frame_free
+    // @return frame 
+    AVFrame* CreateFrame();
+
+    ///////////////////////////////////////////////////////////////
+    /// 编码数据 每次新创建AVPacket
+    /// @para frame 空间由用户维护
+    /// @return 失败范围nullptr 返回的AVPacket用户需要通过av_packet_free 清理
+    AVPacket* Encode(const AVFrame *frame);
+
+    //////////////////////////////////////////////////////////////
+    //返回所有编码缓存中AVPacket
+    std::vector<AVPacket*> End();
 
     //////////////////////////////////////////
     /// 设置对象的编码器上下文 上下文传递到对象中，资源由XEncode维护
